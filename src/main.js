3ec76e5b870e8227c4b5b88ee9b16393b681f238
viacheslav <gv@betwinneraffiliates.com>
2019-02-12 09:11:02 +0300

    gallery

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './scss/main.scss';
+import 'bootstrap';
 import 'owl.carousel'
 import i from './js/index.js';
 
@@ -6,8 +7,61 @@ import i from './js/index.js';
 $(document).ready(function () {
   i.scroll('#headerButtonId', '#servicesId'); // func(button, to)
   i.clickTab('#aboutTabId', '#aboutBlockId'); // func(tabs, block) 
-  $('.owl-carousel').owlCarousel({
+
+  $('#owl').owlCarousel({
     autoPlay: 3000, //Set AutoPlay to 3 seconds
-    items: 6
+    items: 6,
+    nav: true,
   });
+
+  $('#owl img').click(function () {
+    const src = $(this).attr('src');
+    const owlGallery = $('#owl-modal');
+    owlGallery.empty();
+    const arr = [];
+    let startFrom;
+
+    $('#owl img').each(function (i, e) {
+      const otherSrc = $(e).attr('src');
+      if (src === otherSrc) {
+        startFrom = i;
+
+      };
+      arr.push(otherSrc);
+    });
+
+    let tail = []
+    arr.forEach((e, i) => {
+      if (i >= startFrom) {
+        $('<img>', { 'class': 'item', 'src': e }).appendTo(owlGallery);
+      } else {
+        tail.push(e)
+      }
+    });
+    tail.forEach((e, i) => {
+      $('<img>', { 'class': 'item', 'src': e }).appendTo(owlGallery);
+    });
+
+    const gal = owlGallery.owlCarousel({
+      singleItem: true,
+      items: 1,
+      navigation: false,
+    });
+
+    gal.on('initialized.owl.carousel', function () {
+      console.log('hrllo');
+
+      owlGallery.show();
+
+    })
+
+    $('#modal').unbind().on('hidden.bs.modal', function () {
+      gal.trigger('destroy.owl.carousel');
+      owlGallery.fadeIn();
+    });
+  })
+
+
+
+
 }) 
